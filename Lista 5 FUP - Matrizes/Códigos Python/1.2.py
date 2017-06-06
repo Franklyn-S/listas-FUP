6d78419ae880e56796230f7942d2aed2f00f36f4
Franklyn Seabra <franklynseabra12@hotmail.com>
2017-06-06 14:48:51 -0300

    Pasta da lista 5 com o exercício 1.2

---
+++ Lista 5 FUP - Matrizes/Códigos Python/1.2.py
@@ -0,0 +1,29 @@
+'''
+Esse código cria uma matriz 4x4 onde os valores são escolhidos pelo usuário
+e mostra a sua diagonal principal
+
+Autor: Franklyn Seabra
+Cursando Ciências da computação no 1º semestre
+'''
+#Cria uma lista para a matriz e uma lista que irá conter a diagonal
+m = []
+diagonalP = [0,0,0,0]
+
+#preenche a lista com os valores do usuário
+for i in range(0,4):
+	m.append([])
+	for j in range(0,4):
+		m[i].append(int(input("Digite a valor da linha %i com a coluna %i: " % (i+1, j+1))))
+
+#imprime a lista		
+for k in range(0,4):
+	print(m[k])
+
+#Coloca os valores da Diagonal da matriz na lista DiagonalP
+for i in range(0,4):
+	for j in range(0,4):
+		if i == j:
+			diagonalP[i] = m[i][j]
+#imprime a diagonal
+print("As diagonais são: ")
+print (diagonalP)
